1f836e8d434c660fe4d70d51d50030cf9e8f758e
Zack Carson <zack.carson@gmail.com>
2023-08-03 18:43:53 -0600

    Initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float minY = 15f;
+
+    GameSession gameSession;
+    BlackHole ball;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<BlackHole>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 paddlePos = new Vector2(GetPaddleXPosition(), transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(paddlePos.x, minX, minY);
+
+        transform.position = paddlePos;
+	}
+
+    private float GetPaddleXPosition()
+    {
+        // If the ball has been destroyed, we need to find it again!
+        if (ball != null)
+        {
+            if (gameSession.IsAutoPlayEnabled() && ball.HasGameStarted())
+            {
+                return ball.transform.position.x;
+            }
+            else
+            {
+                return screenWidthUnits * Input.mousePosition.x / Screen.width;
+            }
+        }
+        else
+        {
+            ball = FindObjectOfType<BlackHole>();
+
+            return screenWidthUnits * Input.mousePosition.x / Screen.width;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    GameSession gameStatus;
+
+    private void Start()
+    {
+        gameStatus = FindObjectOfType<GameSession>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void LoadNextScene(float delay)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        StartCoroutine(LoadNextSceneAfterDelay(delay, currentSceneIndex));
+    }
+
+    public void LoadFirstScene()
+    {
+        gameStatus.ResetGame();
+
+        int startSceneIndex = 0;
+
+        SceneManager.LoadScene(startSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadNextSceneAfterDelay(float delay, int currentSceneIndex)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
